f35591fc839b7f993c1861168138681db8f3bdb8
ggomezz7 <gabrielgmox@gmail.com>
2026-05-05 19:01:55 -0300

    Adicionando calcular pib per capita e densidade populacional.

--- supertrunfo/super_trunfo.c
+++ supertrunfo/super_trunfo.c
@@ -2,9 +2,9 @@
 
     int main(){
 
-        printf("-----------------------------\n");
+        printf("***__________Programa para comparar uma cidade com a outra__________***\n");
 
-        printf("Qual o estado?: ");
+        printf("\nQual o estado?: ");
         char estado[30];
         scanf("%[^\n]", estado); // Lê o nome do estado
         
@@ -32,17 +32,23 @@
         int pontos_turisticos;
         scanf("%d", &pontos_turisticos); // Lê a quantidade de pontos turísticos da cidade
 
-        printf("---------------------------------\n");
+        float densidade_populacional = populaçao / area;
+        float pib_per_capita = pib / populaçao;
+
+
+        printf("--------------------------------------\n");
 
         printf("O seu estado é: %s.\n", estado);
         printf("O seu código é: %s.\n", cod_carta);
         printf("A cidade do seu estado é %s.\n", cidade);
         printf("A população da sua cidade é: %d \n", populaçao);
         printf("A área da sua cidade é: %.2f\n", area);
-        printf("O pib da sua cidade é %.2f\n", pib);
+        printf("O pib da sua cidade é %.2f reais.\n", pib);
         printf("O número de pontos turísticos é: %d\n", pontos_turisticos);
+        printf("O pib per capita da sua cidade é: %.2f reais.\n", pib_per_capita);
+        printf("A densidade populacional da sua cidade é: %.0f hab/km².\n", densidade_populacional);
 
-        printf("------------------------------------\n");
+        printf("--------------------------------------\n");
 
         // Informações carta 2.
 
@@ -73,19 +79,25 @@
         printf("Qual o número de pontos turísticos da segunda cidade?: ");
         int pontos_turisticos2;
         scanf("%d", &pontos_turisticos2);
+
+        float densidade_populacional2 = populaçao2 / area;
+        float pib_per_capita2 = pib2 / populaçao2;
     
-        printf("-----------------------------\n");
+        printf("--------------------------------------\n");
     
         printf("O seu estado 2 é: %s.\n", estado2);
         printf("O seu código 2 é: %s.\n", cod_carta2);
         printf("A cidade do seu estado 2 é: %s.\n", cidade2);
         printf("A população da sua cidade 2 é: %d \n", populaçao2);
         printf("A área da sua cidade 2 é: %.2f\n", area2);
-        printf("O pib da sua cidade 2 é: %.2f\n", pib2);
+        printf("O pib da sua cidade 2 é: %.2f reais.\n", pib2);
         printf("O número de pontos turísticos 2 é: %d\n", pontos_turisticos2);
-        printf("------------------------------------\n");
+        printf("O pib per capita da sua segunda cidade é: %.2f reais.\n", pib_per_capita2);
+        printf("A densidade populacional da sua segunda cidade é: %.0f hab/km²\n", densidade_populacional2);
 
-        if (populaçao > populaçao2) {
+        printf("--------------------------------------\n");
+
+        if (populaçao > populaçao2) 
             printf("A população da primeira cidade é maior do que a segunda.\n");
         } else if (populaçao < populaçao2) {
             printf("A população da segunda cidade é maior do que a primeira.\n");
@@ -117,7 +129,7 @@
             printf("O número de pontos turísticos das duas cidades é igual.\n");
         }
 
-        printf("\n-----------------------------\n");
+        printf("\n---------------------------------------------------\n");
 
         return 0;
     }   
